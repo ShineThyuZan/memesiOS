5059b2df22c0fc49ea4ff634a25a7f280207af3e
lwinmoeaunganbuchi <lwinmoeaung.anbuchi@gmail.com>
2024-04-04 17:14:27 +0700

    Initial Commit

---
+++ api_setup/api_setupApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_setupApp.swift
+//  api_setup
+//
+//  Created by lwinmoeaung on 04/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct api_setupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
